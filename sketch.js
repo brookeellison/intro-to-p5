e9b7c0ebac140096ad7e7db763805f7887a5aeef
Brooke Ellison <bellison@wlapcs.org>
2017-03-01 20:25:19 +0000

    first commit

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup(){
+    createCanvas(600,400);
+    background(100,100,100);
+}
+
+
+function draw(){
+    line(35,80,400,200);
+    line(50,80,250,300);
+    line(400,20,60,20);
+    
+    line(200,100,200,300);
+    
+    
+    fill(255,0,0);
+    stroke(4);
+    stroke(255,255,255);
+    fill(255,0,0);
+    rect(290,100,200,250);
+    rect(250,150,200,290);
+    stroke(0,0,0);
+    strokeWeight(4);
+    fill(0,0,255);
+    ellipse(50,250,80,80);
+    ellipse(200,40,50,30);
+    ellipse(300,55,30,30);
+    fill(255,0,0);
+    fill(255,0,0);
+    
+    
+    
+    
+}
